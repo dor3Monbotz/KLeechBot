1b1e6780cafe7c4ea9d48ba5496005487fbb53a4
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-09-02 22:34:22 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -108,7 +108,7 @@ if DATABASE_URL:
         del qbit_opt['_id']
         qbit_options = qbit_opt
     conn.close()
-    BOT_TOKEN = environ.get('BOT_TOKEN', '')
+    BOT_TOKEN = environ.get('BOT_TOKEN', '6663148754:AAHTgrKazeWdlj2XBCIzAVHsmRDti5F61ZA')
     bot_id = BOT_TOKEN.split(':', 1)[0]
     DATABASE_URL = environ.get('DATABASE_URL', '')
 else:
