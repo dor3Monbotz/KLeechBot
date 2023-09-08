32619bee48dde9eadeec1228463b0cc693daa3b9
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-09-08 21:56:43 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -2,14 +2,14 @@
 class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = 'Repo'
-    ST_BN1_URL = 'https://www.github.com/weebzone/WZML-X'
-    ST_BN2_NAME = 'Updates'
-    ST_BN2_URL = 'https://t.me/WZML_X'
+    ST_BN1_NAME = 'Owner 🥷'
+    ST_BN1_URL = 'https://t.me/SexyNano'
+    ST_BN2_NAME = 'Updates 🕹'
+    ST_BN2_URL = 'https://t.me/JujutsuHighNetwork'
     ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
 <b>Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
-    ST_UNAUTH = '''<i>You Are not authorized user! Deploy your own WZML-X Mirror-Leech bot</i>'''
+    ST_UNAUTH = '''<i>You Are not authorized user! Deploy your own Leech bot</i>'''
     # ---------------------
 
     # async def stats(client, message):
